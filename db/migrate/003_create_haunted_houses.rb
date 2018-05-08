9d2cdc13127acbfbbb648c2c42ecf04f2d1572b6
Rebecca Tong <rtong@buffalo.edu>
2018-05-08 19:32:50 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,15 @@
 # Create your haunted_houses migration here
 
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :haunted_houses do
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+    end
+  end
+  end
 end
