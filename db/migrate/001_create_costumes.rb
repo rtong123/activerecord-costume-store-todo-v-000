a1df9c88d80102d35f1dfc9fcced07ac5efe6c58
Rebecca Tong <rtong@buffalo.edu>
2018-05-08 19:21:22 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,8 +13,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
     create_table :costumes do |t|
       t.string :name
       t.integer :price
-      t.string :size
       t.string :image_url
+      t.string :size
       t.timestamps
     end
   end
