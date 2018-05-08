8046db25075a594c807f87b62f3a63716647cf58
Rebecca Tong <rtong@buffalo.edu>
2018-05-08 19:28:05 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,8 +8,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.string :still_in_business
-      t.datetime :opening
-      t.datetime :closing
+      t.datetime :opening_time
+      t.datetime :closing_time
       t.timestamps
     end
   end
